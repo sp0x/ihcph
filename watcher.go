999c02d3bc114eb5ec9763def02d9389653f2021
Vasil <vaskovasilev94@yahoo.com>
2020-06-26 08:20:57 +0200

    Single runs now show all results, instead of just the first one.

--- watcher.go
+++ watcher.go
@@ -50,17 +50,20 @@ func loadTelegram() {
 }
 
 func broadcastResults(resultsChan <-chan search.ExternalResultItem) {
-	select {
-	case result := <-resultsChan:
-		link := result.Site
-		availableTime := result.GetField("time")
-		msgText := fmt.Sprintf("I found a new opening at %s:\t%s\n", link, availableTime)
-		message := &bots.ChatMessage{Text: msgText, Banner: result.Banner}
-		telegram.Broadcast(message)
-	case <-time.After(10 * time.Second):
-		fmt.Printf("Timed out waiting for result")
-		os.Exit(1)
+	for {
+		select {
+		case result := <-resultsChan:
+			link := result.Site
+			availableTime := result.GetField("time")
+			msgText := fmt.Sprintf("I found a new opening at %s:\t%s\n", link, availableTime)
+			message := &bots.ChatMessage{Text: msgText, Banner: result.Banner}
+			telegram.Broadcast(message)
+		case <-time.After(10 * time.Second):
+			fmt.Printf("Timed out waiting for result")
+			os.Exit(1)
+		}
 	}
+
 }
 
 //Reads the channel that's the result of watching an indexer.
