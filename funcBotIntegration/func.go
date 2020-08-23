6d8499c867d0e23053881adf97d63a7d3428640a
Vasil <vaskovasilev94@yahoo.com>
2020-08-23 21:29:03 +0200

    chore: bugfix

--- funcBotIntegration/func.go
+++ funcBotIntegration/func.go
@@ -3,7 +3,9 @@ package funcBotIntegration
 import (
 	"cloud.google.com/go/firestore"
 	"context"
+	log "github.com/sirupsen/logrus"
 	"github.com/sp0x/ihcph/common"
+	"github.com/spf13/viper"
 )
 
 var initialized = false
@@ -14,19 +16,24 @@ type Context struct {
 	ctx      context.Context
 }
 
-func Initialize() *Context {
+func Initialize() (*Context, error) {
 	if initialized {
-		return globalContext
+		return globalContext, nil
 	}
+	log.SetLevel(log.InfoLevel)
 	common.BindConfig()
+	verboseValue := viper.GetString("VERBOSE")
+	if verboseValue != "" {
+		log.SetLevel(log.DebugLevel)
+	}
 	initialized = true
 	ctxt := &Context{}
 	fbase, err := common.NewFirebaseFromEnv()
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 	ctxt.Firebase = fbase
 	ctxt.ctx = context.Background()
 	globalContext = ctxt
-	return ctxt
+	return ctxt, nil
 }
--- go.mod
+++ go.mod
@@ -27,6 +27,7 @@ require (
 	golang.org/x/net v0.0.0-20200602114024-627f9648deb9 // indirect
 	golang.org/x/sys v0.0.0-20200620081246-981b61492c35 // indirect
 	google.golang.org/api v0.28.0
+	google.golang.org/grpc v1.28.0
 	google.golang.org/protobuf v1.24.0 // indirect
 	gopkg.in/ini.v1 v1.57.0 // indirect
 )
